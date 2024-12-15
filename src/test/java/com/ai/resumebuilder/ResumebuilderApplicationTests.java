3da991530606ac95eebb86afb46dcc23909a5ada
Ankit Tandon <Ankit.me7@gmail.com>
2024-12-15 14:55:38 +0530

    First commit

---
+++ src/test/java/com/ai/resumebuilder/ResumebuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.resumebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumebuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
